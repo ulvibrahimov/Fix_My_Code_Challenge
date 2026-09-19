fd2f33e37d592ad01bb0ecbd5e419be00c58b8a7
ulvibrahimov <ulvibrahimov@github.com>
2026-09-19 17:17:32 +0000

    Add missing C helper files for Task 4

---
+++ challenge/4-delete_dnodeint/add_dnodeint_end.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - adds a new node at the end of a dlistint_t list
+ * @head: pointer to pointer to the head of the list
+ * @n: integer value for new node
+ * Return: address of new element, or NULL if failed
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new, *last;
+
+	new = malloc(sizeof(dlistint_t));
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = NULL;
+
+	if (*head == NULL)
+	{
+		new->prev = NULL;
+		*head = new;
+		return (new);
+	}
+
+	last = *head;
+	while (last->next)
+		last = last->next;
+
+	last->next = new;
+	new->prev = last;
+
+	return (new);
+}
